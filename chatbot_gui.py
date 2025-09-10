163d6adaf9c8912463cfae17f8b508a7a1229065
Amit Tapria <amit.tapria@walmart.com>
2025-09-10 19:23:14 +0000

    Finished The Pretrained Model Bot

--- chatbot_gui.py
+++ chatbot_gui.py
@@ -5,6 +5,26 @@ from datetime import datetime
 from tkinter import *
 
 
+import os
+import transformers
+
+
+def initialize_model():
+
+  model = transformers.pipeline("conversational", model="facebook/blenderbot_small-90M")
+  os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+  return model
+
+
+def get_bot_response(model, user_input):
+
+  chat = model(transformers.Conversation(user_input))
+  bot_response = str(chat)
+  bot_response = bot_response[bot_response.find("bot >> ")+6:].strip()
+
+  return bot_response
+
 def create_and_insert_user_frame(user_input):
   userFrame = Frame(chatWindow, bg="#d0ffff")
   Label(
